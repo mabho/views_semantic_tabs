469561454cec31d461b859253abe93deeda48e5c
krknth <krknth@gmail.com>
2016-02-11 17:30:30 +0530

    Custom js, css code for working with tabs.

---
+++ js/views-semantic-tabs.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $(document).ready(function() {
+        $(".tabs-menu a").click(function(event) {
+            event.preventDefault();
+            $(this).parent().addClass("current");
+            $(this).parent().siblings().removeClass("current");
+            var tab = $(this).attr("href");
+            $(".tab-content").not(tab).css("display", "none");
+            $(tab).fadeIn();
+            console.log(tab)
+        });
+    });
+})(jQuery);
