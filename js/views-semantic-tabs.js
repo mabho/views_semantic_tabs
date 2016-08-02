9153905d728508383efde61593b4803c0e5169ec
Dom <dom.clause@gmail.com>
2016-08-02 22:00:58 +0200

    Issue #2777795 by Dom.: JS should run in a Drupal behavior

--- js/views-semantic-tabs.js
+++ js/views-semantic-tabs.js
@@ -1,5 +1,12 @@
-(function ($) {
-    $(function() {
-        $( ".views-semantic-tabs" ).tabs();
-    });
-})(jQuery);
+/**
+ * @file
+ * Applies tabs on views semantic tabs display.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.views_semantic_tabs = {
+    attach: function (context) {
+      $( ".views-semantic-tabs" ).tabs();
+    }
+  };
+})(jQuery, Drupal);
